e10445be612da59ba52e51cd0d8382e50d50a938
florian <florianmarquier1@gmail.com>
2023-10-13 14:42:21 +0200

    main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
-jhgyfpljhvb
+#include <iostream>
+#include "Heros.h"
+
+using namespace std;
+
+int main()
+{
+
+    Heros yoda("Maitre Yoda", 260,"Sabre Laser",50);
+    Heros luke("Luke Skywalker", 450, "Sabre Laser", 50);
+    Heros leia("Princesse Leia", 100, "Pistolet Laser", 30);
+    Heros solo("Han Solo", 150, "Pistolet Laser",30);
+    Heros palpatine("Empereur Palpatine",250, "Sabre Laser", 50);
+    Heros vador("Dark Vador", 400, "Sabre Laser", 50);
+    Heros r2d2("R2D2", 50, "Pistolet Laser", 30);
+    Heros c3po("C-3PO", 80, "Laser", 30);
+
+    retunr 0;
+}
