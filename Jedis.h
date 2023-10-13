eb29bdebd219514216631c82cfdc2c7ae1bc411b
florian <florianmarquier1@gmail.com>
2023-10-13 15:41:44 +0200

    modif Jedis

--- Jedis.h
+++ Jedis.h
@@ -6,6 +6,8 @@
 class Jedis : public Heros
 {
     public:
+        Jedis(std::string nomheros, int vieheros, std::string nomarme, int puissancearme);
+        Jedis(std::string nomheros, int vieheros, std::string nomarme, int puissancearme, int force);
         void utiliserLaForce();
     private:
     int m_force;
