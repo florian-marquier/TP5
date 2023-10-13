1c3e226b7fe8975c3254a31bd390a96b6efd6592
florian <florianmarquier1@gmail.com>
2023-10-13 15:05:50 +0200

    Heros.h

---
+++ Heros.h
@@ -0,0 +1,22 @@
+#ifndef Heros_h
+#define Heros_h
+
+#include <iostream>
+#include <string>
+
+class Heros
+{
+    private:
+        std::string m_nomheros;
+        int m_vieheros;
+        std::string m_nomarme;
+        int m_puissancearme;
+    public:
+        Heros();
+        Heros(std::string nomheros, int vieheors, std::string nomarme, int puissancearme);
+        void diminuerVie(int vieenmoins);
+        void attaquer(Heros&cible);
+        bool vivant() const;
+        void afficher()const;
+};
+#endif
