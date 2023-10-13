f38b5fe8cac11605985061f9502e5753fe6b7711
florian <florianmarquier1@gmail.com>
2023-10-13 16:17:37 +0200

    Siths cpp

---
+++ Siths.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "Siths.h"
+#include <string>
+
+using namespace std;
+
+Siths::Siths(string nomheros, int vieheros, string nomarme, int puissancearme) : Heros(nomheros, vieheros, nomarme, puissancearme), m_coteobscur(80)
+{
+
+}
+
+Siths::Siths(string nomheros, int vieheros, string nomarme, int puissancearme) : Heros(nomheros, vieheros, nomarme, puissancearme), m_coteobscur(80)
+{
+
+}
